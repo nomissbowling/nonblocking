67ff5703d54a2c2632b9020aa62d93f051997afb
nomissbowling <nomissbowling@yahoo.co.jp>
2023-06-20 12:34:25 +0900

    version 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/nonblock/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/nonblock/0.1.1")]
 /// nonblocking stdin
 
 pub mod nb_stdin;
--- src/nb_stdin.rs
+++ src/nb_stdin.rs
@@ -2,29 +2,59 @@
 
 use std::{thread, time};
 use std::sync::mpsc;
+use std::error::Error;
 
+use futures::StreamExt;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use crossterm::event; // event::{poll, read};
-// use crossterm::event::{Event, KeyCode};
+use crossterm::event::{Event, EventStream, KeyCode, KeyModifiers};
+// use crossterm::execute;
 
 // use crossbeam_channel::{Sender, Receiver, unbounded, tick};
 
 /// NbStdin
 pub struct NbStdin {
+  reader: EventStream
 }
 
 impl NbStdin {
 
+/// constructor
 pub fn start() -> Self {
-  let nb = NbStdin{};
+  let nb = NbStdin{reader: EventStream::new()};
   enable_raw_mode().unwrap();
   nb
 }
 
+/// destructor
 pub fn stop(&self) {
   disable_raw_mode().unwrap();
 }
 
+/// async-await
+pub async fn async_stdin(&mut self) -> Result<bool, Box<dyn Error>> {
+  loop {
+    match self.reader.next().await {
+    None => break,
+    Some(Err(e)) => { println!("Error: {:?}", e); break; },
+    Some(Ok(e)) => {
+      match e {
+      Event::Key(ke) => {
+        if ke.code == KeyCode::Esc { return Ok(true); }
+        if ke.modifiers == KeyModifiers::CONTROL {
+          if ke.code == KeyCode::Char('C') { return Ok(true); }
+        }
+      },
+      _ => ()
+      }
+      break;
+    }
+    }
+  }
+  Ok(false)
+}
+
+/// another thread
 pub fn non_block_stdin(timeout: time::Duration) -> bool {
   let (tx, rx) = mpsc::channel();
   let _handle = thread::spawn(move || {
