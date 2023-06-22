5cddb5997404e9a384fb908aceea50108c91c73f
nomissbowling <nomissbowling@yahoo.co.jp>
2023-06-22 15:26:44 +0900

    version 0.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
-#![doc(html_root_url = "https://docs.rs/nonblock/0.1.2")]
+#![doc(html_root_url = "https://docs.rs/nonblock/0.1.3")]
 //! nonblocking stdin
 
 pub mod nb_stdin;
+
+#[cfg(test)]
+mod tests {
+  use super::nb_stdin::NbStdin;
+  use std::time;
+
+  #[test]
+  fn a_test() {
+    let nb = NbStdin::start();
+    let b = NbStdin::non_blocking_stdin(time::Duration::from_millis(20),
+      NbStdin::break_with_esc);
+    let a = NbStdin::async_non_blocking_stdin(time::Duration::from_millis(20),
+      NbStdin::break_with_esc);
+    nb.stop();
+    assert_eq!(b, false);
+    assert_eq!(a, false);
+  }
+}
--- src/nb_stdin.rs
+++ src/nb_stdin.rs
@@ -4,7 +4,9 @@ use std::{thread, time};
 use std::sync::mpsc;
 use std::error::Error;
 
+use futures::{select, pin_mut, future::FutureExt};
 use futures::StreamExt;
+use futures::executor::block_on;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use crossterm::event; // event::{poll, read};
 use crossterm::event::{Event, EventStream, KeyCode, KeyModifiers};
@@ -32,31 +34,82 @@ pub fn stop(&self) {
   disable_raw_mode().unwrap();
 }
 
+/// default for async_stdin
+pub fn break_with_esc(e: Event) -> bool {
+  match e {
+  Event::Key(ke) => {
+    if ke.code == KeyCode::Esc { return true; }
+    if ke.modifiers == KeyModifiers::CONTROL {
+      if ke.code == KeyCode::Char('c') { return true; } // ^c
+    }
+    if ke.modifiers == KeyModifiers::CONTROL | KeyModifiers::SHIFT {
+      if ke.code == KeyCode::Char('C') { return true; } // ^c with shift
+    }
+    ()
+  },
+  _ => ()
+  }
+  false
+}
+
 /// async-await
-pub async fn async_stdin(&mut self) -> Result<bool, Box<dyn Error>> {
-  loop {
-    match self.reader.next().await {
-    None => break,
-    Some(Err(e)) => { println!("Error: {:?}", e); break; },
-    Some(Ok(e)) => {
-      match e {
-      Event::Key(ke) => {
-        if ke.code == KeyCode::Esc { return Ok(true); }
-        if ke.modifiers == KeyModifiers::CONTROL {
-          if ke.code == KeyCode::Char('C') { return Ok(true); }
-        }
-      },
-      _ => ()
-      }
-      break;
-    }
-    }
+pub async fn async_stdin(&mut self, lambda: fn (e: Event) -> bool) -> Result<bool, Box<dyn Error>> {
+  match self.reader.next().await { // blocking
+  None => (),
+  Some(Err(e)) => { println!("Error: {:?}", e); () },
+  Some(Ok(e)) => { if lambda(e) { return Ok(true); } }
   }
   Ok(false)
 }
 
+/// select async-await
+pub async fn select_stdin() -> Result<Option<Event>, Box<dyn Error>> {
+  let mut reader = EventStream::new();
+  loop {
+    let c = reader.next().fuse();
+    pin_mut!(c);
+    select! {
+      e = c => {
+        match e {
+        None => (),
+        Some(Err(e)) => { println!("Error: {:?}", e); () },
+        Some(Ok(e)) => { return Ok(Some(e)); }
+        }
+      },
+      complete => break,
+      default => break // or () to block in the loop or unreachable!()
+    }
+  }
+  Ok(None)
+}
+
+/// async another thread
+pub fn async_non_blocking_stdin(timeout: time::Duration, lambda: fn (e: Event) -> bool) -> bool {
+  let (tx, rx) = mpsc::channel();
+  let _handle = thread::spawn(move || {
+    loop {
+      match block_on(NbStdin::select_stdin()).unwrap() { // blocking
+      None => (),
+      Some(e) => {
+        match tx.send(e) {
+        Ok(()) => break,
+        Err(_) => ()
+        }
+      }
+      }
+    }
+    ()
+  });
+  match rx.recv_timeout(timeout) {
+  Ok(e) => { if lambda(e) { return true; } },
+  Err(mpsc::RecvTimeoutError::Timeout) => (),
+  Err(mpsc::RecvTimeoutError::Disconnected) => { println!("disconnected"); () } // unreachable!()
+  }
+  false
+}
+
 /// another thread
-pub fn non_block_stdin(timeout: time::Duration) -> bool {
+pub fn non_blocking_stdin(timeout: time::Duration, lambda: fn (e: Event) -> bool) -> bool {
   let (tx, rx) = mpsc::channel();
   let _handle = thread::spawn(move || {
     loop {
@@ -76,13 +129,9 @@ pub fn non_block_stdin(timeout: time::Duration) -> bool {
     ()
   });
   match rx.recv_timeout(timeout) {
-  Ok(e) => {
-    match e {
-    _ => ()
-    }
-  },
+  Ok(e) => { if lambda(e) { return true; } },
   Err(mpsc::RecvTimeoutError::Timeout) => (),
-  Err(mpsc::RecvTimeoutError::Disconnected) => unreachable!()
+  Err(mpsc::RecvTimeoutError::Disconnected) => { println!("disconnected"); () } // unreachable!()
   }
   false
 }
